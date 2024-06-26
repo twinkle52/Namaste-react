302a49f2abc748ead03db1737335f2dfa14ac099
twinkle52 <jaiswaltwinkle52@gmail.com>
2024-06-26 23:57:37 +0530

    second episode

--- main.js
+++ main.js
@@ -1,26 +1,16 @@
+import React from "react"; // importing React from node modules
+import ReactDOM from "react-dom/client"; // in the index.html adding type as module because it was giving error as
+// "Browser scripts cannot have imports or exports."
+// browser is taking the script as normal browser script. write module so that browser can consider it as module.
+//Now our react will come from node modules
+//    <script type= "modules" src="./main.js"></script>
 const parent = React.createElement(
   "div",
   { id: "parent" },
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "heading1" }, "Sibling 1!"),
+    React.createElement("h1", { id: "heading1" }, "Sibling 1"),
     React.createElement("h1", { id: "heading2" }, "Sibling 2"),
   ])
 );
 console.log(parent);
-const root = ReactDOM.createRoot(document.getElementById("root")); //craeteRppt assigns the root for React.
-root.render(parent); // here render replace the element alraedy present in the root, not append but replace
-
-// React only works inside root, thats why its a library, react can work in small piece of appliactiona nd in large also
-// parent is a object.
-// so React.craeteElement creates a object(child sibling parant all is object),which is a react element that gets converted to HTML, that browser understands
-// we know that html elemnt has a tree structue so how we can create that in React.
-/**
- * <div>
- *   <div>
- *     <h1><h1/>
- *   </div>
- * <div>
- */
-// if i have to create a sibling, i need a array
-// creating the element with this way will create a complex code and not readable, Yes the above is the code of React.
-// but we are not going to use createElement of the raect, we will use JSX.
+const root = ReactDOM.createRoot(document.getElementById("root"));
--- readme.md
+++ readme.md
@@ -1,1 +1,45 @@
 # hello
+
+# open index.html
+
+# node preparation.js
+
+First we had the CDN url for react(not a good way, the url may change) now we will import react from NPM
+
+# npm init
+
+We will get package.json which is a configuration for npm.
+Bundler bundles our app compress it clean it so that it can be shipped to production. ex (webpack, parcel these are bundler).
+They ignite our app.
+
+# npm install -D parcel
+
+- install parcel dev dependency(need while development) not a normal dependency(alraedy in production)
+  devDependencies in json is development env dependency
+  ^ carret - update the version minor better to use it.
+  ~ tilde - update the version major
+  package lock json keeps the exact verion of the dependency, while package json has tilde and carret
+
+# npm install -D parcel
+
+- when ran this packagelock.json came and node modules came, node modules is all the dependencies one will require for development from npm.
+  If i have package and packagelock.json, i can reh=generate my node modules, just do npm instal. So git will also generate.
+
+# npm install react and npm instal react dom
+
+to get the react in our code
+
+# parcel https://parceljs.org/docs/ chapter 2 all about parcel
+
+- Dev build
+- local server host app to server
+- HMR (Hot module replacement)
+- Image optimization
+- bundling as it is a bundler
+- minification
+- compress
+- file watching algo
+- onsistent hashing
+- differential bundling - so that it con support old, new browser, mobile and every thing
+
+Why React is fast because of these bundlers like parcel.
