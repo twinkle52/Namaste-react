a28cc645a8ea2d83b06112a44e186250466b6596
twinkle52 <jaiswaltwinkle52@gmail.com>
2024-07-17 12:07:15 +0530

    AllAboutClassComponentAndItsMethod

--- src/components/About.js
+++ src/components/About.js
@@ -1,53 +1,20 @@
 import { Component, useEffect } from "react";
 import UserClass from "./UserClass";
-import User from "./User";
 
-// class About extends Component {
-//   constructor(props) {
-//     super(props);
-//     console.log("Parent constructor called");
-//   }
-//   componentDidMount() {
-//     console.log("parent useEffect");
-//   }
-//   render() {
-//     console.log("parent render called");
+class About extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-//     return (
-//       <div>
-//         <h1>About Class component</h1>
-//         <h2>This is Namaste React Web Series</h2>
-//         <UserClass
-//           name={"Twinkle Jaiswal(Class)"}
-//           location={"Bangalore class"}
-//         />
-//       </div>
-//     );
-//   }
-// }
-
-const About = () => {
-  console.log("parent componet");
-
-  useEffect(() => {
-    console.log("parent useEffect");
-  });
-  return (
-    <div>
-      <h1>About Class component</h1>
-      <h2>This is Namaste React Web Series {console.log("parent return")}</h2>
-      <User name={"first(Class)"} location={"Bangalore class"} />
-      <User name={"second(Class)"} location={"Pune class"} />
-    </div>
-  );
-};
+  render() {
+    return (
+      <div>
+        <h1>About Class component</h1>
+        <h2>This is Namaste React Web Series</h2>
+        <UserClass />
+      </div>
+    );
+  }
+}
 
 export default About;
-
-// same is the life cycle for useeffect as well.
-// parent componet
-// parent return
-// child component
-// child return
-// User // child useEffect
-// About parent useEffect
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,32 +4,68 @@ class UserClass extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0,
+      userInfo: {
+        name: "Dummy",
+        location: "Dummy",
+      },
     };
+    console.log("constructor");
   }
 
-  componentDidMount() {}
+  async componentDidMount() {
+    const data = await fetch("https://api.github.com/users/twinkle52");
+    const json = await data.json();
+    this.setState({
+      userInfo: json,
+    });
+    console.log("componentDidMount");
+  }
+
+  componentDidUpdate() {
+    console.log(
+      "components gets updated after the api is fetched and DOM manipulation is done with new data"
+    );
+  }
+
+  componentWillUnmount() {
+    console.log(
+      "This will be called when the page gets changes say from about to contactus"
+    );
+  }
   render() {
-    const { name, location } = this.props;
+    const { name, location, company, avatar_url } = this.state.userInfo;
+    console.log("rendered");
 
     return (
       <div className="user-card">
-        <h1>Count: {this.state.count}</h1>
-        <button
-          onClick={() => {
-            this.setState({
-              count: this.state.count + 1,
-            });
-          }}
-        >
-          button
-        </button>
-        <h1>Name: {this.props.name}</h1>
-        <h2>Location: {this.props.location}</h2>
-        <h3>Contact: @twinkle123</h3>
+        <img src={avatar_url} />
+        <h1>Name: {name}</h1>
+        <h2>Location: {location}</h2>
+        <h3>Company: {company}</h3>
       </div>
     );
   }
 }
 
 export default UserClass;
+
+/**
+ * Life Cycle of the React
+ *
+ * --Mounting----
+ *
+ * Constructor(Dummy)
+ * Render(Dummy)
+ *
+ *     <HTML With Dummy>
+ * Component Did Mount
+ *      <API Call>
+ *      <this,.setState> -> state variable is updated
+ *
+ * ---- Update ---
+ *
+ *    Render(API new data, no constructor now)
+ *    <HTML new API data>
+ *     component Did update
+ *
+ */
